fcf14a7f422d865a874290cbcecfdc4d942040d7
diegodev14 <diegodev14@outlook.com>
2026-04-30 15:27:45 -0500

    chore: eliminar docker y ajustar entorno local

--- app/Config/database.php
+++ app/Config/database.php
@@ -14,11 +14,11 @@ $capsule = new Capsule();
 
 $capsule->addConnection([
     'driver' => $_ENV['DB_CONNECTION'] ?? 'mysql',
-    'host' => $_ENV['DB_HOST'] ?? 'mysql',
+    'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
     'port' => (int) ($_ENV['DB_PORT'] ?? 3306),
     'database' => $_ENV['DB_DATABASE'] ?? 'inventario_ventas',
-    'username' => $_ENV['DB_USERNAME'] ?? 'inventario_user',
-    'password' => $_ENV['DB_PASSWORD'] ?? 'inventario_pass',
+    'username' => $_ENV['DB_USERNAME'] ?? 'root',
+    'password' => $_ENV['DB_PASSWORD'] ?? '',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => '',
